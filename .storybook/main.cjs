88a5a1a7355c6aa41871a18c528ad113ff23e38a
Wemerson <wemersoncontas22@gmail.com>
2022-10-17 01:30:59 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/Rignite-lab-03/'
+      config.base = '/ignite-lab-03/'
     }
 
     return config
